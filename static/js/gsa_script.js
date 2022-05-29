9ebce269e7b4882e66b72272385a6119ad1ee524
Arty <colomborin53@gmail.com>
2022-05-29 17:52:32 +0300

    Sound affects added

--- static/js/gsa_script.js
+++ static/js/gsa_script.js
@@ -1,19 +1,58 @@
-document.addEventListener("load", () => {
-  start_btn = document.getElementById("start-btn");
-  start_btn.onclick = () => {
-    navigator.mediaDevices.getUserMedia({audio:true, video: false})
-    .then(handleSuccess, reason = () =>{
-      console.log("An error ocurred");
-    })
-  }
+start_btn = document.getElementById("start-btn");
+let tunesRetrieved = false;
+let tunes;
+start_btn.onclick = () => {
+console.log("Clicked");
+  navigator.mediaDevices.getUserMedia({audio:true, video: false})
+  .then(handleSuccess);
+}
 
-  class Thone{
-    constructor(){
-      
+class Thone{
+//Audio element that can be played
+  #audioElement;
+  //audio context
+  #audioCtx;
+  //source node for the tune
+  #audioSourceNode;
+
+  constructor(audioElementName){
+    this.#audioCtx = new AudioContext();
+    try{
+      this.#audioElement = new Audio(audioElementName);
+      this.#audioElement.repeat = false;
+      this.#audioSourceNode = this.audioCtx.createMediaElementSource(this.#audioElement);
+
+    }catch(e){
+      console.log(e);
     }
   }
 
-  function handleSuccess(){
-
+  playAudio(){
+    this.#audioElement.play();
   }
-})
+}
+
+
+
+function handleSuccess(){
+  if(!tunesRetrieved){
+    tunes = {
+      D: new Thone("/media/tunes/d3.mp3"),
+      A: new Thone("/media/tunes/a2.mp3"),
+      El: new Thone("/media/tunes/e2.mp3"),
+      G: new Thone("/media/tunes/g3.mp3"),
+      B: new Thone("/media/tunes/b3.mp3"),
+      Er: new Thone("/media/tunes/e4.mp3")};
+      tunesRetrieved = true;
+  }
+
+  start_btn.innerText = "Sound Enabled";
+
+  strings = document.querySelectorAll(".tune");
+  console.log(strings);
+  for(let string of strings){
+    string.onclick = () => {
+      tunes[string.id].playAudio();
+    };
+  }
+}
