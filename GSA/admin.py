2fb6776a711929b4a96e94baedd0543266a23909
Arty <colomborin53@gmail.com>
2022-06-11 01:13:38 +0300

    added illustration model, seems works fine

--- GSA/admin.py
+++ GSA/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from .models import Instrument, Sound
+from .models import Instrument, Sound, Illustration
 
 
 @admin.register(Instrument)
 class InstrumentAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(Sound)
 class SoundAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Illustration)
+class IllustrationAdmin(admin.ModelAdmin):
     pass
--- GSA/models.py
+++ GSA/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
+class Illustration(models.Model):
+    path = models.CharField(max_length=200)
+    name = models.CharField(max_length=80)
+    single = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.path[self.path.rfind("/"):]
+
+
 class Instrument(models.Model):
     name = models.CharField(max_length=100)
     link = models.CharField(max_length=100)
-    picture_path = models.CharField(max_length=100)
+    picture = models.ForeignKey(Illustration, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -12,7 +21,7 @@ class Instrument(models.Model):
 class Sound(models.Model):
     sound_name = models.CharField(max_length=50)
     sound_number = models.IntegerField()
-    sound_path = models.CharField(max_length=100)
+    sound_path = models.CharField(max_length=200)
     instrument = models.ForeignKey(Instrument, on_delete=models.CASCADE)
 
     def __str__(self):
