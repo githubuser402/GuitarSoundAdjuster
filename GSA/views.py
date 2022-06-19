768d58d25aaba01104aff1e8cc30f058adb362f1
githubuser402 <colomborin53@gmail.com>
2022-06-19 16:33:34 +0300

    added help page

--- GSA/views.py
+++ GSA/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from django.views.generic import ListView, View
 from .models import Instrument, Illustration
 import random
@@ -12,26 +12,27 @@ class GSAList(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         query =  Illustration.objects.filter(single=False)
-        print(query)
         context['background'] = random.choice(query)
         return context
 
 
 class GSADetailedView(View):
     def get(self, request, **kwargs):
-        if(kwargs["link"] == "help"):
-            return HttpResponse("Its help message")
-
         queryset = {}
+
+        if(kwargs["link"] == "help"):
+            query =  Illustration.objects.filter(single=False)
+            queryset['background'] = random.choice(query)
+            queryset['title'] = """Help Message"""
+            queryset['message'] = """Now you are using GSA - Guitar Sound Adjuster"""
+            return render(request, "gsa_app/help.html", queryset)
+
         
         instrument = Instrument.objects.filter(link=kwargs["link"])
         if(instrument.count() != 0):
             instrument = instrument[0]
             sounds = instrument.sound_set.all().order_by("sound_number")
-
             queryset["instrument"] = instrument
             queryset["sounds"] = sounds
             return render(request, "instrument/instrument_detail.html", queryset)
 
-        else:
-            return HttpResponse("This type of instrument is not in list")
